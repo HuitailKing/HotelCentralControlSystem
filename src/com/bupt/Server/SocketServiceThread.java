d9fd655271865fa1d6ee112a2ea60d32bbb8e098
dongguanchen <dongguanchen@bytedance.com>
2019-03-24 16:07:30 +0800

    Tcp thread pool

---
+++ src/com/bupt/Server/SocketServiceThread.java
@@ -0,0 +1,45 @@
+package com.bupt.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServiceThread implements Runnable{
+    private String ServiceName ;
+    private ServerSocket server;
+    private int port=8888;
+    public SocketServiceThread(String ServiceName){
+        this.ServiceName = "ServiceThread---"+ServiceName+"---:";
+        this.init(ServiceName);
+    }
+    public void init(String portOffset){
+        try {
+            server = new ServerSocket(port+Integer.parseInt(portOffset));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run(){
+        try {
+            System.out.println(ServiceName+"Server start!");
+            Socket socket = server.accept(); //阻塞等待, 直到一个客户端 socket过来
+            System.out.println(ServiceName+"There comes a socket!");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream())); //输入，from 客户端
+            PrintWriter out = new PrintWriter(socket.getOutputStream()); //输出，to 客户端
+            System.out.println(ServiceName+in.readLine());  // 打印 客户 socket 发过来的字符，按行(\n,\r,或\r\n)
+            System.out.println(ServiceName+in.readLine());
+            out.println(ServiceName+"Server reponse! ^_^ ");
+            out.flush(); // to 客户端，输出
+            socket.close();
+            server.close();
+            System.out.println(ServiceName+"Server end!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
